00361c325cdf2b7ef5bf91694bb0ebe22a8a1d8f
Carlos Reynaga <carandreyn@gmail.com>
2022-10-26 23:54:42 -0700

    cards app initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {title: "Neat Tree", imageURL: "assets/tree.jpeg", username: "nature", content: "I saw this neat tree today."},
+    {title: "Snowy Mountain", imageURL: "assets/mountain.jpeg", username: "mountainlover", content: "Here is a picture of snowy mountain."},
+    {title: "Mountain Biking", imageURL: "assets/biking.jpeg", username: "biking12222", content: "I did some biking today."},
+  ];
 }
